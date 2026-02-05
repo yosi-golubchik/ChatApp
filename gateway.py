6699df902e20682ca281b919f46044d20b970a23
yosi-golubchik <yosi.golubchik@gmail.com>
2026-02-05 14:58:05 +0200

    Added GET /history/{room_id} route to get all messages from a chatroom

--- gateway.py
+++ gateway.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends
 from src.config import input_topic
 from src.kafka import get_producer
 from src.models import Message
+from src.redis import get_redis_client
 
 app = FastAPI()
 
@@ -19,3 +20,21 @@ async def send_message(message: Message, producer: Producer = Depends(get_produc
     producer.poll(0)
 
     return {'status': 'sent'}
+
+@app.get("/history/{room_id}")
+async def get_history(room_id: str) -> list[dict]:
+    redis = get_redis_client()
+    key = f"room_history:{room_id}"
+
+    raw_messages = redis.xrange(key, min="-", max="+")
+
+    history = []
+    for msg_id, data in raw_messages:
+        history.append({
+            "id": msg_id,
+            "sender": data.get("user"),
+            "content": data.get("msg"),
+            "timestamp": msg_id.split("-")[0]
+        })
+
+    return history
